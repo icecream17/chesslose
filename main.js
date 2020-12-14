531fd390e5c5a982f8daa07b91bff9e28bceadce
steven nguyen <58114641+icecream17@users.noreply.github.com>
2020-12-13 21:33:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,175 @@
+let chessboard = Chessboard('board1', getBoardConfig())
+let chessgame = new Chess()
+
+// for async functions
+async function pause(ms) {
+   return await new Promise(resolve => setTimeout(resolve, ms, "Done!"));
+}
+
+function getBoardConfig() {
+   function preventIllegalStart(source, piece, position, orientation) {
+      let legalMoves = chessgame.moves({ verbose: true });
+      return legalMoves.some(move => move.from === source)
+   }
+
+   function preventIllegalMove(source, target) {
+      let legalMoves = chessgame.moves({ verbose: true }).filter(move => move.from === source && move.to === target);
+
+      if (legalMoves.length === 0) return 'snapback';
+      else if (legalMoves.length === 1) chessgame.move(legalMoves[0]);
+      else if (legalMoves.length > 1) {
+         const promotion = prompt("Choose promotion letter: \n\n" + JSON.stringify(legalMoves, null, 3))
+         if (chessgame.move({
+            from: source,
+            to: target,
+            promotion
+         }) === null) return 'snapback';
+      } else throw RangeError("Less than 0 legal moves?");
+   }
+
+   return {
+      pieceTheme: 'alpha/{piece}.png',
+      draggable: true,
+      position: "start",
+      onDragStart: preventIllegalStart,
+      onDrop: preventIllegalMove,
+      onSnapEnd: updateBoard
+   }
+}
+
+function updateBoard () {
+   chessboard.position(chessgame.fen())
+   console.clear()
+   console.log(chessgame.fen())
+   console.log(chessgame.ascii())
+}
+
+let nets = [];
+for (let i = 0; i < 100; i++) nets.push(new Net(i))
+
+let round = 0;
+let gameID = 0;
+let playerIDs = [0, 1];
+globalThis.nets = nets;
+updateTextarea();
+
+document.getElementById('start').onclick = async function () {
+   while (true) {
+      while (!chessgame.game_over()) {
+         let input = await getInput();
+         if (chessgame.turn() === chessgame.WHITE) {
+            let output = await nets[playerIDs[0]].run(input)
+            await doMove(output);
+         } else {
+            let output = await nets[playerIDs[1]].run(input)
+            await doMove(output);
+         }
+
+         await updateTextarea();
+         await pause(100);
+      }
+
+      await pause(250);
+      if (chessgame.in_draw()) {
+         nets[playerIDs[0]].score[playerIDs[1]] = 0.5;
+         nets[playerIDs[1]].score[playerIDs[0]] = 0.5;
+      } else if (chessgame.turn() === chessgame.WHITE) {
+         nets[playerIDs[0]].score[playerIDs[1]] = 1;
+         nets[playerIDs[1]].score[playerIDs[0]] = 0;
+      } else {
+         nets[playerIDs[0]].score[playerIDs[1]] = 0;
+         nets[playerIDs[1]].score[playerIDs[0]] = 1;
+      }
+
+      playerIDs[1]++;
+      gameID++;
+
+      if (playerIDs[1] === 100) {
+         playerIDs[0]++;
+         playerIDs[1] = 0;
+      }
+
+      if (playerIDs[0] === playerIDs[1]) playerIDs[1]++
+      if (playerIDs[1] === 100) break;
+
+      await updateTextarea();
+      chessgame.reset();
+   }
+
+   round++;
+}
+
+function updateTextarea () {
+   document.getElementById('info').value = `Round ${round}
+Game #${gameID}
+Net ${playerIDs[0]} vs Net ${playerIDs[1]}
+
+0: ${nets[0].toString()}
+1: ${nets[1].toString()}
+
+${chessgame.pgn()}`
+}
+
+async function getInput() {
+   let input = []
+   for (let i of chessgame.board()) {
+      for (let j of i) {
+         if (j === null) {
+            input.push(0); continue;
+         }
+
+         let value = 0;
+         if (j.type === chessgame.PAWN) value = 1 / 6;
+         if (j.type === chessgame.KNIGHT) value = 2 / 6;
+         if (j.type === chessgame.BISHOP) value = 3 / 6;
+         if (j.type === chessgame.ROOK) value = 4 / 6;
+         if (j.type === chessgame.QUEEN) value = 5 / 6;
+         if (j.type === chessgame.KING) value = 1;
+
+         if (j.color === chessgame.BLACK) value *= -1;
+         input.push(value)
+      }
+   }
+
+   return input
+}
+
+async function doMove(output) {
+   function toNumber(movePart) {
+      return (("abcdefgh".indexOf(movePart[0]) * 8) + Number(movePart[1]) - 32) / 64
+   }
+
+   function promotionNumber (val) {
+      if (val === undefined) return 0;
+      else if (val === 'n') return -1;
+      else if (val === 'b') return -0.5;
+      else if (val === 'r') return 0.5;
+      else if (val === 'q') return 1;
+   }
+
+   let moves = chessgame.moves({ verbose: true });
+   let movesToNumber = []
+
+   for (let move of moves) {
+      movesToNumber.push([toNumber(move.from), toNumber(move.to), promotionNumber(move?.promotion)])
+   }
+
+   let best = [Infinity, moves[0]]
+   for (let i = 0; i < movesToNumber.length; i++) {
+      let num = movesToNumber[i]
+
+      let cost = 0;
+      cost += Math.abs(num[0] - output[0])
+      cost += Math.abs(num[1] - output[1])
+      cost += 0.01 * Math.abs(num[2] - output[2])
+
+      if (best[0] > cost) {
+         best = [cost, moves[i]]
+      }
+   }
+
+   chessgame.move(best[1])
+   chessboard.position(chessgame.fen())
+
+}
+
---
+++ net.js
@@ -0,0 +1,131 @@
+/**
+ * Gets a random integer between minimum inclusive, and maximum exclusive,
+ * @param {number} min - The minimum integer possible, inclusive.
+ * @param {number} max - The maximum integer possible, exclusive.
+ * @returns {number} - A random integer between min (inclusive) and max (exclusive)
+ * @example randomInteger(18, 29) // 23
+ * randomInteger(32879, 893) // 28359
+ * randomInteger(0.2, -5) // -4
+ */
+function randomInteger(min, max) {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+/**
+ * Gets a random number between minimum inclusive, and maximum exclusive,
+ * @param {number} min - The minimum number possible, inclusive.
+ * @param {number} max - The maximum number possible, exclusive.
+ * @returns {number} - A random number between min (inclusive) and max (exclusive)
+ * @example randomInRange(18, 29) // 18.02358678329
+ * randomInRange(32879, 893) // 5952.9056785728
+ * randomInRange(0.2, -5) // -4.4368949634 (WARNING: Never above 0)
+ */
+function randomInRange(min, max) {
+   return Math.random() * (max - min) + min;
+}
+
+function squishify(value) {
+   return Math.tanh(value);
+}
+
+globalThis.Neuron = class Neuron {
+   #value = randomInRange(-1, 1);
+   #bias = randomInRange(-1, 1);
+
+   constructor (nn, layer, indexOfLayer, indexInLayer) {
+      this.nn = nn;
+      this.layer = layer;
+      this.indexOfLayer = indexOfLayer;
+      this.indexInLayer = indexInLayer;
+   }
+
+   get value () {
+      if (this.indexOfLayer === 0) return this.#value + this.#bias
+
+      let val = 0
+      for (let i = 0; i < this.nn.layers[this.indexOfLayer - 1].neurons.length; i++) {
+         let neuron = this.nn.layers[this.indexOfLayer - 1].neurons.length;
+         val += neuron * this.layer.receivingWeights[i][this.indexInLayer];
+      }
+
+      val += this.#bias;
+      this.#value = squishify(val)
+      return Promise.resolve(this.#value);
+   }
+
+   set value (val) {this.#value = val;}
+}
+
+globalThis.Layer = class Layer {
+   constructor(nn, index, numNeurons = randomInteger(9, 256)) {
+      this.nn = nn;
+
+      /** Index of layer in nn */
+      this.index = index;
+
+      this.neurons = [];
+      for (let i = 0; i < numNeurons; i++) {
+         this.neurons.push(new Neuron(nn, this, index, i))
+      }
+
+      this.receivingWeights = null;
+      if (index !== 0) {
+         this.receivingWeights = [];
+         for (let i = 0; i < this.nn.layers[index - 1].length; i++) {
+            this.receivingWeights[i] = []
+
+            for (let j = 0; j < this.neurons.length; j++) {
+               this.receivingWeights[i].push(randomInRange(-1, 1))
+            }
+         }
+      }
+   }
+
+   get length () {return this.neurons.length}
+
+   *[Symbol.iterator]() {
+      throw Error("Iterate over the neurons or weights instead of the layer")
+   }
+}
+
+globalThis.Net = class Net {
+   #id = null;
+   constructor (id) {
+      this.#id = id;
+      this.layers = [];
+
+      this.layers.push(new Layer(this, 0, 64));
+      for (let i = 1; i < randomInteger(3, 7); i++) {
+         this.layers.push(new Layer(this, i));
+      }
+
+      this.layers.push(new Layer(this, this.layers.length, 3));
+
+      this.lastOut = null
+      this.score = [];
+   }
+
+   get id () {return this.#id;}
+   get totalScore () {return this.score.reduce((accum, curr) => accum + curr, 0)}
+
+   async run (...inputs) {
+      for (let i = 0; i < inputs.length; i++) this.layers[0][i] = inputs[i];
+
+      let out = []
+      for (let outNode of this.layers[this.layers.length - 1].neurons) {
+         out.push(await outNode.value)
+      }
+
+      this.lastOut = out;
+      return out;
+   }
+
+   toString() {
+      let str = `[object Net] score: ${this.totalScore}\nx`
+      str += this.layers.map(layer => layer.length).join()
+      str += ` - ${this.lastOut}`
+      return str
+   }
+}
